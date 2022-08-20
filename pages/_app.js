d405eb3829fdf4be0037b9fa53426d406f706a2a
DhruvKaushal <dhruvkaushal1997@gmail.com>
2022-08-20 13:39:29 +0530

    Set up project, include eslint, header

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import Header from './header';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+        <title>GameZop Assignment</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    <Header></Header>
+      </>
+  )
 }
 
 export default MyApp
