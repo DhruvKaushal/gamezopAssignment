b7dd1507332e7a47b9bd2a7862678a7218096612
DhruvKaushal <dhruvkaushal1997@gmail.com>
2022-08-20 16:24:58 +0530

    styled news section

--- pages/news.js
+++ pages/news.js
@@ -2,11 +2,14 @@ import DataCard from "./components/dataCard";
 import styles from "../styles/news.module.css"
 function News({ data }) {
   return (
+      <div style={{marginTop:'60px'}}>
+      <br/>
+      <h2 style={{textAlign:'center'}}>News feed</h2>
       <div className={styles.newsContainer}>
-      <br></br>
       {data.map((item) => (
         <DataCard key={item.id} item={item}/>
       ))}
+      </div>
       </div>
   )
 }
